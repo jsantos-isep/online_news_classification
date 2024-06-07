f37743a8f5e2af6ea27f5525c1a4d04ba49540d3
Joana Santos <ffs@isep.ipp.pt>
2024-06-07 17:35:52 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,3 @@
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
 
-setup(name='news_classification', version='1.0', packages=find_packages())
+setup(name="news_classification", version="1.0", packages=find_packages())
