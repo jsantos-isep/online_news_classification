d6d7cb27502700bdf7b4ff1d6ac697932e433878
Joana Santos <ffs@isep.ipp.pt>
2024-06-14 11:58:06 +0100

    Remove the unused function parameter "args".

--- online_news_classification/convert_datasets/the_guardian_to_csv.py
+++ online_news_classification/convert_datasets/the_guardian_to_csv.py
@@ -8,7 +8,7 @@ from online_news_classification.functions import manage_datasets, setup
 load_dotenv()
 
 
-def convert(f, args, output_file):
+def convert(f, output_file):
     dataset = manage_datasets.read_json_dataset(filename=f)
     data = pd.json_normalize(dataset["results"][0], max_level=1)
     data["title"] = data["fields.headline"]
@@ -56,11 +56,11 @@ def main():
             if filename.endswith(".json"):
                 f = os.path.join(args.input, filename)
                 output_file = os.path.join(args.output, os.path.splitext(filename)[0])
-                convert(f, args, output_file)
+                convert(f, output_file)
     else:
         filename = os.path.basename(args.input)
         output_file = os.path.join(args.output, os.path.splitext(filename)[0])
-        convert(args.input, args, output_file)
+        convert(args.input, output_file)
     setup.finalize(start_time)
 
 
